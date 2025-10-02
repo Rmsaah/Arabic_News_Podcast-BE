c168ccd288daff1d52036601f4dfbdc60535d971
Rmsaah <rmsaah@hotmail.com>
2025-10-02 16:34:20 +0300

    pushing local project to github.

---
+++ src/main/java/com/shakhbary/arabic_news_podcast/ArabicNewsPodcastBeApplication.java
@@ -0,0 +1,13 @@
+package com.shakhbary.arabic_news_podcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArabicNewsPodcastBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArabicNewsPodcastBeApplication.class, args);
+	}
+
+}
