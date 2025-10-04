6d548fe638e2afccd78e7d5f74986f4fa1fe1407
Rmsaah <rmsaah@hotmail.com>
2025-10-04 17:50:27 +0300

    created exception handlers

---
+++ src/main/java/com/shakhbary/arabic_news_podcast/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,5 @@
+package com.shakhbary.arabic_news_podcast.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) { super(message); }
+}
